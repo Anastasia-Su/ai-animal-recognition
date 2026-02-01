3c6652f86f15e6b49b391f210a05e95167ae14ad
Anastasia <anastasiiya.p@gmail.com>
2026-02-01 23:15:57 +0200

    Removed comment

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ app.config["RESTX_MASK_SWAGGER"] = False
 app.register_blueprint(router)
 
 setup_api(app)
-# api = Api(
-#     app,
-#     version="1.0",
-#     title="Animal Classifier API",
-#     doc="/docs"
-# )
-# # Define a namespace
-# ns = api.namespace("predict", description="ML prediction operations")
-
 
 if __name__ == "__main__":
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
